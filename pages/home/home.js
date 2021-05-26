21e56be9dac3d68b0811881e2da328507f5ac8a2
HanssyCheung <15660959229@163.com>
2021-05-26 11:08:58 +0800

    预备知识

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,23 +1,23 @@
-// pages/home/home.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
 
-  },
+  Page({
+  
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        students: [
+          {id: 110 ,name: 'swk',age: 18},
+          {id: 111 ,name: 'zbj',age: 19},
+          {id: 112 ,name: 'shs',age: 20},
+          {id: 113 ,name: 'blm',age: 13}
+        ],
+        counter: 0
+    },
+    handleBtnClick() {
+      // //错误的做法，界面不会自动刷新
+      // this.data.counter += 1
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
-})
+      //正确的做法：this.setData()
+      this.setData({counter: this.data.counter + 1})
+    }
+  })
